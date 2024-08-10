239d11f69d4c7fc2f1244de9513c094f7a7fb4b3
Madurainhire <108894773+rohidygy@users.noreply.github.com>
2024-08-10 15:05:43 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-TOKEN = os.getenv("TOKEN")
+API_ID = int(os.getenv("API_ID", "25194940"))
+API_HASH = os.getenv("API_HASH", "ed16f15b65c372cc60159afb62963824")
+TOKEN = os.getenv("TOKEN", "6450071825:AAFb1qVXPZaJPKa4A9Ekel8CkS8YuBwLc-8")
