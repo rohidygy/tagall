0b2d769a5c85511d8edc8fae15734545bf15878b
Madurainhire <108894773+rohidygy@users.noreply.github.com>
2025-08-19 22:26:32 +0700

    Update pler.py

--- pler.py
+++ pler.py
@@ -60,7 +60,7 @@ async def payment24_callback(event):
         "💳 **Silahkan lakukan pembayaran melalui akses berikut:**\n\n"
         "🏧 **BCA:** `8520330721 A/n M STEAPHEN`\n"
         "🏧 **BRI:** `011201106024509 A/n NICHOLAS`\n"
-        "🏧 **BNI:** `188-652-0309 A/n YOSE RIZAL"
+        "🏧 **BNI:** `188-652-0309 A/n YOSE RIZAL`\n"
     )
 
     await event.edit(
@@ -75,18 +75,18 @@ async def payment24_callback(event):
 @kntl.on(events.CallbackQuery(data=b"payment12"))
 async def payment12_callback(event):
     text = (
-        "💎 **VIP 12 JAM:**\n\n"
-        "💵 **Harga:** `Rp. 80.000`\n\n"
+        "💎 **KODE AKSES 10 HARI**\n\n"
+        "💵 **Harga:** `Rp 50.000`\n\n"
         "💳 **Silahkan lakukan pembayaran melalui akses berikut:**\n\n"
-        "🪙 **Dana:** `081234567890`\n"
-        "🏧 **BCA:** `999000028`\n"
-        "👤 Atas Nama: **Dana**"
+        "🏧 **BCA:** `8520330721 A/n M STEAPHEN`\n"
+        "🏧 **BRI:** `011201106024509 A/n NICHOLAS`\n"
+        "🏧 **BNI:** `188-652-0309 A/n YOSE RIZAL`\n"
     )
 
     await event.edit(
         text,
         buttons=[
-            [Button.url("Konfirmasi Pembayaran", "t.me/kagebunshiiin")],
+            [Button.url("Konfirmasi Pembayaran", "t.me/telegalerinakal_bot")],
             [Button.inline("Kembali", b"back_to_menu")],
         ],
     )
@@ -95,18 +95,18 @@ async def payment12_callback(event):
 @kntl.on(events.CallbackQuery(data=b"payment6"))
 async def payment6_callback(event):
     text = (
-        "💎 **VIP 6 JAM:**\n\n"
-        "💵 **Harga:** `Rp. 50.000`\n\n"
+        "💎 **KODE AKSES 14 HARI**\n\n"
+        "💵 **Harga:** `Rp 100.000`\n\n"
         "💳 **Silahkan lakukan pembayaran melalui akses berikut:**\n\n"
-        "🪙 **Dana:** `081234567890`\n"
-        "🏧 **BCA:** `999000028`\n"
-        "👤 Atas Nama: **Dana**"
+        "🏧 **BCA:** `8520330721 A/n M STEAPHEN`\n"
+        "🏧 **BRI:** `011201106024509 A/n NICHOLAS`\n"
+        "🏧 **BNI:** `188-652-0309 A/n YOSE RIZAL`\n"
     )
 
     await event.edit(
         text,
         buttons=[
-            [Button.url("Konfirmasi Pembayaran", "t.me/kagebunshiiin")],
+            [Button.url("Konfirmasi Pembayaran", "t.me/telegalerinakal_bot")],
             [Button.inline("Kembali", b"back_to_menu")],
         ],
     )
@@ -115,18 +115,19 @@ async def payment6_callback(event):
 @kntl.on(events.CallbackQuery(data=b"payment3"))
 async def payment3_callback(event):
     text = (
-        "💎 **VIP 12 JAM:**\n\n"
-        "💵 **Harga:** `Rp. 35.000`\n\n"
+        "💎 **VCS TALENT GALERI NAKAL**\n\n"
+        "💵 **Harga:** `Rp 100.000`\n\n"
         "💳 **Silahkan lakukan pembayaran melalui akses berikut:**\n\n"
-        "🪙 **Dana:** `081234567890`\n"
-        "🏧 **BCA:** `999000028`\n"
-        "👤 Atas Nama: **Dana**"
+        "🏧 **BCA:** `8520330721 A/n M STEAPHEN`\n"
+        "🏧 **BRI:** `011201106024509 A/n NICHOLAS`\n"
+        "🏧 **BNI:** `188-652-0309 A/n YOSE RIZAL`\n"
     )
 
     await event.edit(
         text,
         buttons=[
-            [Button.url("Konfirmasi Pembayaran", "t.me/kagebunshiiin")],
+            [Button.url("Konfirmasi Pembayaran", "t.me/telegalerinakal_bot")],
+            [Button.url("Admin Galeri Nakal", "t.me/amiragalerinakal")],
             [Button.inline("Kembali", b"back_to_menu")],
         ],
     )
@@ -279,3 +280,4 @@ async def mentionalls(event):
 
 print("BOT AKTIF KONTOL")
 kntl.run_until_disconnected()
+
